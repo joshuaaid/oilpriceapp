b73c3ee044e6e810aaa191528bf0f50fe067042d
Joshgun Jafarov <108449849+joshuaaid@users.noreply.github.com>
2023-12-20 13:03:18 +0400

    Update app.py

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ max_date = mydata['date'].max()
 #start_date = st.date_input("Please define start date:", min_value=mydata['date'].min())
 #end_date = st.date_input("Please define end date:",  max_value= mydata['date'].max(), value = default_date)
 
-start_date = st.date_input("Please define start date:", min_value=min_date)
-end_date = st.date_input("Please define end date:",  max_value=max_date )
+start_date = st.date_input("Please define start date:", min_value = min_date)
+end_date = st.date_input("Please define end date:",  min_value = min_date, max_value = max_date )
 
 if start_date > end_date:
     st.warning("Start date cannot be greater than end date.")
