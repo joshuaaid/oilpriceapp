a16b5f90875d7e6a3608a3a07460a0d0c5012678
Joshgun Jafarov <108449849+joshuaaid@users.noreply.github.com>
2023-12-27 15:35:05 +0400

    Update stock.py

--- pages/stock.py
+++ pages/stock.py
@@ -20,16 +20,17 @@ def main():
     else:
         symbols = ["TSLA", "AAPL","AMD","PYPL","META","GOOG","MSFT","JPM","WMT","XOM","JNJ"]
         symbol_filter = st.multiselect('Please select the stocks to visualize:', symbols, default=['AAPL'])
-        data = yf.download(symbol_filter, start=start_date, end=end_date)
-        adj_close_data = data['Adj Close']
-        fig1 = px.line(adj_close_data, labels={'value': 'Adjusted Close Price','variable' : 'Stock'})
-        st.plotly_chart(fig1)
-          # Data Source
-        st.markdown("---")
-        st.markdown("### Data Source:")
-        st.markdown("<div> The data used is taken live from <a href='https://finance.yahoo.com/'> finance.yahoo.com </a>. The stock prices are updated regularly and in real-time. </div>",
+        if not symbol_filter:
+            st.warning("Please select at least one stock.")
+        else:
+            data = yf.download(symbol_filter, start=start_date, end=end_date)
+            adj_close_data = data['Adj Close']
+            fig1 = px.line(adj_close_data, labels={'value': 'Adjusted Close Price','variable' : 'Stock'})
+            st.plotly_chart(fig1)
+              # Data Source
+            st.markdown("---")
+            st.markdown("### Data Source:")
+            st.markdown("<div> The data used is taken live from <a href='https://finance.yahoo.com/'> finance.yahoo.com </a>. The stock prices are updated regularly and in real-time. </div>",
                 unsafe_allow_html=True)
-
-
 if __name__ == "__main__":
     main()
